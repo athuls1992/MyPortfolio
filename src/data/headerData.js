afc343b816a6dcaa4ef651632fbc593607eab8ac
Athul Sreenivasan <52868837+athuls1992@users.noreply.github.com>
2024-02-04 10:40:29 +0300

    Update headerData.js
    Title changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import image from '../assets/png/insta_pic.png'
 
 export const headerData = {
     name: 'Athul S',
-    title: "Web Developer",
+    title: "Software Architect UI",
     desciption:"There wasn't a bird in the sky, but that was not what caught his attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. He knew what was coming and he hoped he was prepared. ",
     image: image,
     resumePdf: resume
