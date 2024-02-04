197addde1ff85e6a79b287432f8290d271b8aa36
Athul Sreenivasan <52868837+athuls1992@users.noreply.github.com>
2024-02-04 10:54:04 +0300

    Update achievementData.js
    Certifications

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -3,14 +3,22 @@ export const achievementData = {
     achievements : [
         {
             id : 1,
+            title : 'Associate Cloud Engineer-Google Cloud',
+            details : 'Cloud Engineer deploys applications, monitors operations, and manages enterprise solutions on GCP.',
+            date : 'Jan 26, 2024',
+            field : 'Devops/Cloud',
+            image : 'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/93305378'
+        },
+         {
+            id : 2,
             title : 'Mastering React with Redux',
             details : 'Certificate received on completion of course from Udemy.com - A website for interactive Learning.',
-            date : 'Jan 20, 20122',
+            date : 'Jan 20, 2019',
             field : 'Programming',
             image : 'https://www.udemy.com/staticx/udemy/js/webpack/dual-logos-desktop.0e03506fb02a4ce3073097e3fc035bba.svg'
         },
         {
-            id : 2,
+            id : 3,
             title : 'Developing Web applications with php and mySQL',
             details : 'Issued by NIIT Technologies.',
             date : 'Aug 20, 2013',
@@ -18,7 +26,7 @@ export const achievementData = {
             image : 'https://www.niit.com/en/learning-outsourcing/themes/niit/logo.svg'
         },
         {
-            id : 3,
+            id : 4,
             title : 'Google Adwords & Analytics',
             details : 'Google Certified Digital Marketing Professional',
             date : 'Aug 20, 2017',
