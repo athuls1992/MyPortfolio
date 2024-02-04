96dd66cb0e33b5e93dd4e3ff2ab9d2fb9c4001fb
Athul Sreenivasan <52868837+athuls1992@users.noreply.github.com>
2024-02-04 10:56:28 +0300

    Update aboutData.js
    ABOUT

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Athul. I'm a web designer and developer based in Doha, Qatar.",
-    description2: "During the day I work as a lead developer at a Loop Mobility and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours with my baby boy and family. Right now I'm also trying a hand at Robotics and AI. I love to learn and explore new arenas.",
+    description1: "My name's Athul. I'm a Software architect and Engineer based in Doha, Qatar.",
+    description2: "During the day I work as a software architect at Qatar Insurance group and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours with my baby boy and family. Right now I'm also trying a hand at ML and AI. I love to learn and explore new arenas.",
     image: 1
 }
