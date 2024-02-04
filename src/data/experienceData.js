07adaead6a84102f5c8ae4d8d02841e374e3dec8
Athul Sreenivasan <52868837+athuls1992@users.noreply.github.com>
2024-02-04 10:45:23 +0300

    Update experienceData.js
    Updated Experience with QIC

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,23 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Protected Harbor Pvt Ltd',
-        jobtitle: 'Software Developer',
-        startYear: '2014',
-        endYear: '2017'
+        company: 'Qatar Insurance Group',
+        jobtitle: 'Principal Software Architect',
+        startYear: '2021',
+        endYear: 'At Present'
     },
     {
         id: 2,
-        company: 'Google Operations Center - Regalix.Inc',
-        jobtitle: 'Software Developer',
-        startYear: '2017',
-        endYear: '2019'
-    },
-    {
-        id: 3,
         company: 'Loop Mobility',
         jobtitle: 'Software Engineer',
         startYear: '2020',
         endYear: '2022'
+    },
+    {
+        id: 3,
+        company: 'Google Operations Center - Regalix.Inc',
+        jobtitle: 'Software Developer',
+        startYear: '2017',
+        endYear: '2019'
     },
 ]
